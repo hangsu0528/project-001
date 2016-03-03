6be9f057f15f6c528f14fcd312cbc1e5f571af49
hangsu0528 <zwang98@ucsc.edu>
2016-03-03 10:38:00 -0800

    updata

--- zhaohuiwangassignment5.R
+++ zhaohuiwangassignment5.R
@@ -23,5 +23,41 @@ graph2+geom_histogram()+facet_grid(cut~.)
 
 graph3<-ggplot(diamonds,aes(x=cut,price))
 graph3+geom_jitter(alpha=0.1)+geom_violin()
+#question2 
+#part a 
+library(foreign)
+require(dplyr)
+org_example <- read.dta("https://github.com/EconomiCurtis/econ294_2015/raw/master/data/org_example.dta")
+
+org_3a <- org_example %>%
+  dplyr::group_by(year,month)%>%
+  dplyr::summarise(
+    rw_quantile1st = quantile(rw, .1, na.rm = T),
+    rw_quantile9st = quantile(rw, .9, na.rm = T),
+    rw_quantile1 = quantile(rw, .25, na.rm = T),
+    rw_quantile3 = quantile(rw, .75, na.rm = T),
+    Median.RW = median(rw, na.rm = T),
+    count = n())
+
+org_3a <- org_3a %>% 
+  mutate(date=paste(year,month,"01", sep="-"),
+         date=as.Date(date,format="%Y-%m-%d"))
+
+part_3a <- ggplot(org_3a, aes(x=date, y=Median.RW))
+part_3a + geom_ribbon(aes(ymin=rw_quantile1, ymax=rw_quantile3),alpha=0.6) + geom_ribbon(aes(ymin=rw_quantile1st, ymax=rw_quantile9st),alpha=0.2) + geom_line(aes(y=Median.RW))+lims(y=c(0,50))
+
+#b#
+org_3b <- org_example %>%
+  dplyr::group_by(year,month,educ)%>%
+  dplyr::summarise(
+    Median.RW = median(rw, na.rm = T),
+    count = n())
+
+org_3b <- org_3b %>%
+  mutate(date=paste(year,month,"01", sep="-"),
+         date=as.Date(date,format="%Y-%m-%d"))
+
+part_3b <- ggplot(org_3b, aes(x=date, y=Median.RW,group=educ))
+part_3b + geom_line(aes(color=educ))
 
 
